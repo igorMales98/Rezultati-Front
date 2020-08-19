319f0d6bbbde7a558b7dbab75b2f602ab1d2efff
igorMales98 <igor_malesevic@yahoo.com>
2020-08-19 22:21:20 +0200

    Prikaz rezultata top 5 liga.

--- src/app/fudbal/fudbal.component.ts
+++ src/app/fudbal/fudbal.component.ts
@@ -15,8 +15,11 @@ import {MatDatepickerInputEvent} from '@angular/material/datepicker';
 })
 export class FudbalComponent implements OnInit {
 
+  selektovanPrikaz = 0;
+
   zemlje: Zemlja[] = [];
   fudbalskiRezultati: FudbalskiRezultat[] = [];
+  top5Rezultati: FudbalskiRezultat[] = [];
 
   zemljeRezultata: Zemlja[] = [];
   ligeRezultata: Liga[] = [];
@@ -75,4 +78,55 @@ export class FudbalComponent implements OnInit {
       this.srediPrikaz();
     });
   }
+
+  tabKlik(tab) {
+    if (tab.index === 0) {
+      this.zemljeRezultata = [];
+      this.ligeRezultata = [];
+      this.ngOnInit();
+    } else if (tab.index === 1) {
+      this.filtrirajTop5();
+    } else if (tab.index === 2) {
+      // kad smislis uzivo sta ce biti
+    }
+  }
+
+  filtrirajTop5() {
+    this.zemljeRezultata = [
+      {id: '26', naziv: 'Engleska', lige: [{id: '42', naziv: 'Premier League', sport: {id: '1', naziv: 'Fudbal'}}], prikazaneLige: false},
+      {id: '85', naziv: 'Nemačka', lige: [{id: '126', naziv: 'Bundesliga', sport: {id: '1', naziv: 'Fudbal'}}], prikazaneLige: false},
+      {id: '32', naziv: 'Francuska', lige: [{id: '188', naziv: 'Ligue 1', sport: {id: '1', naziv: 'Fudbal'}}], prikazaneLige: false},
+      {id: '108', naziv: 'Španija', lige: [{id: '163', naziv: 'LaLiga', sport: {id: '1', naziv: 'Fudbal'}}], prikazaneLige: false},
+      {id: '47', naziv: 'Italija', lige: [{id: '73', naziv: 'Serie A', sport: {id: '1', naziv: 'Fudbal'}}], prikazaneLige: false},
+    ];
+
+    this.ligeRezultata = [
+      {id: '42', naziv: 'Premier League', sport: {id: '1', naziv: 'Fudbal'}},
+      {id: '126', naziv: 'Bundesliga', sport: {id: '1', naziv: 'Fudbal'}},
+      {id: '188', naziv: 'Ligue 1', sport: {id: '1', naziv: 'Fudbal'}},
+      {id: '163', naziv: 'LaLiga', sport: {id: '1', naziv: 'Fudbal'}},
+      {id: '73', naziv: 'Serie A', sport: {id: '1', naziv: 'Fudbal'}},
+    ];
+
+    console.log('treba da prodje');
+    console.log(this.fudbalskiRezultati);
+    this.top5Rezultati = [];
+    for (const rezultat of this.fudbalskiRezultati) {
+      console.log('id lige rez je ' + rezultat.liga.id);
+      for (const liga of this.ligeRezultata) {
+        console.log('id lige  je ' + liga.id);
+
+        // tslint:disable-next-line:triple-equals
+        if (liga.id == rezultat.liga.id) {
+          console.log('isti su');
+          this.top5Rezultati.push(rezultat);
+          break;
+        }
+      }
+    }
+    this.fudbalskiRezultati = this.top5Rezultati;
+    console.log(this.fudbalskiRezultati);
+    console.log('proso je');
+  }
+
 }
